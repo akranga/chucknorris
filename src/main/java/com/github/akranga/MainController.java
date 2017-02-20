04ef81634c4562da8c9dc0775fc2743a9c440823
Dimitry Solovyov <dimitry@solovyov.lv>
2017-02-20 16:37:26 +0200

    Add test

---
+++ src/main/java/com/github/akranga/MainController.java
@@ -0,0 +1,19 @@
+package com.github.akranga;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private FactGenerator factGenerator;
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String gimmeFact() {
+        return factGenerator.gimme() + "\n";
+    }
+}
